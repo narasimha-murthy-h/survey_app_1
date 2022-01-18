1f0e4a930368ba166a4ef56826ea1940bd8c2b6f
Narasimha Murthy H <narasimha.murthy@zymr.com>
2022-01-18 13:11:22 +0530

    Dev-1: Local storage settings

--- src/Survey/index.jsx
+++ src/Survey/index.jsx
@@ -4,69 +4,73 @@ import "./survey.css";
 import titleCard from "../Assets/redBanner.png";
 import { Button } from "../Components/Button";
 import { Radio } from "../Components/Radio";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { FormEvent } from "react";
 import validator from "validator";
 
 const Survey = () => {
 	const [isDisabled, setIsDisabled] = useState(false);
-	const [form, setForm] = useState({
-		name: "",
-		email: "",
-		gender: "Male",
-		age: 0,
-	});
+	const [names, setNames] = useState("");
+	const [age, setAge] = useState(0);
+	const [email, setEmail] = useState("");
+	const [gender, setGender] = useState("Male");
+
+	const getSurveyDetails = () => {
+		const data = localStorage.getItem("Survey_Taken_By");
+		if (data) {
+			return JSON.parse(data);
+		} else {
+			return [];
+		}
+	};
+
+	const [surveyData, setSurveyData] = useState(getSurveyDetails);
 
 	const [error, setError] = useState({
-		name: "",
+		names: "",
 		email: "",
 		age: "",
 	});
 
-	const onChange = (e) => {
-		const { value, name } = e.target;
-		setForm((state) => ({
-			...state,
-			[name]: value,
-		}));
-	};
-
 	const showData = () => {
-		console.log("Form: ", form);
+		console.log("Name: ", names);
+		console.log("Age: ", age);
+		console.log("Gender: ", gender);
+		console.log("Email: ", email);
 	};
-
 	const regex =
 		/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
 
 	const onSubmit = (e) => {
 		e.preventDefault();
 
-		if (!form.name) {
+		if (!names) {
 			setError((state) => ({
 				...state,
-				name: "Please enter a name",
+				names: "Please enter a name",
 			}));
 			return;
 		}
-		if (form.name.length < 5) {
+		if (names.length < 5) {
 			setError((state) => ({
 				...state,
-				name: "Too short",
+				names: "Too short",
 			}));
 			return;
 		} else {
 			setError((state) => ({
 				...state,
-				name: "",
+				names: "",
 			}));
 		}
 
-		if (!form.email) {
+		if (!email) {
 			setError((state) => ({
 				...state,
 				email: "Please enter a mail ID",
 			}));
 			return;
-		} else if (regex.test(form.email) === false) {
+		} else if (regex.test(email) === false) {
 			setError((state) => ({
 				...state,
 				email: "Enter a valid mail ID",
@@ -79,7 +83,13 @@ const Survey = () => {
 			}));
 		}
 
-		if (form.age === 0 || form.age < 1) {
+		if (age === 0 || age < 1) {
+			setError((state) => ({
+				...state,
+				age: "Please enter a proper age",
+			}));
+			return;
+		} else if (age > 100) {
 			setError((state) => ({
 				...state,
 				age: "Please enter a proper age",
@@ -91,18 +101,32 @@ const Survey = () => {
 				age: "",
 			}));
 		}
-
 		showData();
+
+		let survey = { names, age, email, gender };
+		setSurveyData([...surveyData, survey]);
+		setGender("Male");
+		setNames("");
+		setAge(0);
+		setEmail("");
 	};
 
 	const onReset = () => {
-		setForm({
-			name: "",
+		setGender("Male");
+		setNames("");
+		setAge(0);
+		setEmail("");
+		setError({
+			names: "",
 			email: "",
-			gender: "Male",
-			age: 0,
+			age: "",
 		});
 	};
+
+	useEffect(() => {
+		localStorage.setItem("Survey_Taken_By", JSON.stringify(surveyData));
+	}, [surveyData]);
+
 	return (
 		<div className="mainContainer">
 			<div className="box">
@@ -122,24 +146,24 @@ const Survey = () => {
 							<label className="label">
 								Name :
 								<input
-									onChange={onChange}
+									onChange={(e) => setNames(e.target.value)}
 									className="inputContainer"
-									name="name"
-									value={form.name}
+									name="names"
+									value={names}
 									placeholder="Name"
 								/>
 							</label>
-							{!!error.name && <div className="alertText">{error.name}</div>}
+							{!!error.names && <div className="alertText">{error.names}</div>}
 						</div>
 
 						<div>
 							<label className="label">
 								Email :
 								<input
-									onChange={onChange}
+									onChange={(e) => setEmail(e.target.value)}
 									className="inputContainer"
 									name="email"
-									value={form.email}
+									value={email}
 									placeholder="Email"
 								/>
 							</label>
@@ -150,22 +174,22 @@ const Survey = () => {
 							<label className="label">Gender :</label>
 							<span className="inputContainerRadio">
 								<Radio
-									onChange={onChange}
+									onChange={(e) => setGender(e.target.value)}
 									name="gender"
 									value="Male"
-									checked={form.gender === "Male"}
+									checked={gender === "Male"}
 								/>
 								<Radio
-									onChange={onChange}
+									onChange={(e) => setGender(e.target.value)}
 									name="gender"
 									value="Female"
-									checked={form.gender === "Female"}
+									checked={gender === "Female"}
 								/>
 								<Radio
-									onChange={onChange}
+									onChange={(e) => setGender(e.target.value)}
 									name="gender"
 									value="Other"
-									checked={form.gender === "Other"}
+									checked={gender === "Other"}
 								/>
 							</span>
 						</div>
@@ -175,10 +199,10 @@ const Survey = () => {
 								Age :
 								<input
 									type="number"
-									onChange={onChange}
+									onChange={(e) => setAge(e.target.value)}
 									className="inputContainerAge"
 									name="age"
-									value={form.age}
+									value={age}
 									placeholder="Age"
 								/>
 							</label>
